bd6857c313287159fe044cec1fa70b23d4f96d8f
Mika (Admin) Mäki <admin-mika@ad.vexlum.com>
2020-05-20 16:22:22 +0300

    Add Windows Update to the maintenance script

--- maintenance.ps1
+++ maintenance.ps1
@@ -211,6 +211,9 @@ Function Test-CommandExists {
 # Script starts here
 # ---
 
+Install-Module PSWindowsUpdate -Force
+Install-WindowsUpdate -MicrosoftUpdate -IgnoreReboot
+
 if (-Not (Test-CommandExists "choco")) {
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
