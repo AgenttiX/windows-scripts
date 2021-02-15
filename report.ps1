f731eae1cba56b092afe53923d51b089b71f2c17
Mika Mäki <agentti002x@gmail.com>
2021-02-15 19:53:39 +0200

    Add reporting script

---
+++ report.ps1
@@ -0,0 +1,16 @@
+﻿<#
+.SYNOPSIS
+    Create reports on computer status and health
+#>
+
+. "./utils.ps1"
+Elevate($myinvocation.MyCommand.Definition)
+
+New-Item -Path "." -Name "reports" -ItemType "directory" -Force
+
+Write-Host "Creating battery report"
+powercfg /batteryreport /output "./reports/battery.html"
+
+Write-Host "Creating WiFi report"
+netsh wlan show wlanreport
+cp "C:\ProgramData\Microsoft\Windows\WlanReport\wlan-report-latest.html" ./reports
