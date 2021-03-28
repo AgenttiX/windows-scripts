0c3ce6d3c303cb3392e9f7da08b5ba7d2270cebf
Mika Mäki <agentti002x@gmail.com>
2021-03-29 01:20:54 +0300

    Add drive ejection script

---
+++ eject-drive.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+    Eject a disk such as a USB drive safely.
+.LINK
+    https://serverfault.com/a/580298
+.PARAMETER Name
+    Drive letter
+#>
+
+param(
+    [Parameter(Mandatory=$true)][char]$DriveLetter
+)
+
+$driveEject = New-Object -comObject Shell.Application
+$driveEject.Namespace(17).ParseName("${DriveLetter}:").InvokeVerb("Eject")
