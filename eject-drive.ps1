e4c8f8194e077526f8e925b4d3ff87b0f78e4cfa
Mika Mäki <agentti002x@gmail.com>
2021-03-29 18:47:44 +0300

    Fix drive ejection script for Hyper-V Server

--- eject-drive.ps1
+++ eject-drive.ps1
@@ -3,6 +3,7 @@
     Eject a disk such as a USB drive safely.
 .LINK
     https://serverfault.com/a/580298
+    https://community.spiceworks.com/topic/2120667-how-to-safely-remove-usb-key-in-windows-server-core?page=1#entry-7617376
 .PARAMETER Name
     Drive letter
 #>
@@ -11,5 +12,11 @@ param(
     [Parameter(Mandatory=$true)][char]$DriveLetter
 )
 
-$driveEject = New-Object -comObject Shell.Application
-$driveEject.Namespace(17).ParseName("${DriveLetter}:").InvokeVerb("Eject")
+# This works on Windows 10 but seems to have no effect on Hyper-V Server 2019
+# $driveEject = New-Object -comObject Shell.Application
+# $driveEject.Namespace(17).ParseName("${DriveLetter}:").InvokeVerb("Eject")
+
+$vol = Get-WmiObject -Class Win32_Volume | where{$_.Name -eq "${DriveLetter}:\"}
+$vol.DriveLetter = $null
+$vol.Put()
+$vol.Dismount($false, $false)
