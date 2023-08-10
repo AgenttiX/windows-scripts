1b633dfac2ddf959400cf4be332404f5a219aa58
Mika Mäki <mika.maki@vexlum.com>
2023-08-11 00:09:23 +0300

    Add PowerShell profile

---
+++ Profile/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,13 @@
+function Enable-SSHAgent {
+    . "${env:ProgramFiles}\Git\cmd\start-ssh-agent.cmd"
+}
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
---
+++ Profile/Setup-Profile.ps1
@@ -0,0 +1,23 @@
+Set-StrictMode -Latest
+$ErrorActionPreference = "Stop"
+
+. "$((Get-Item ${PSScriptRoot}).Parent.FullName)/utils.ps1"
+
+Show-Output "Configuring PowerShell profile."
+
+$ProfileDir = Split-Path -Path "${profile}" -Parent
+
+$CreateNew = $true
+if (Test-Path -Path "${ProfileDir}") {
+    if ((Get-Item -Path "${ProfileDir}" -Force).LinkType -eq "Junction") {
+        $CreateNew = $false
+    } else {
+        Move-Item -Path "${ProfileDir}" "${ProfileDir}-old"
+    }
+}
+
+if ($CreateNew) {
+    New-item -ItemType "Junction" -Path "${ProfileDir}" -Target "${PSScriptRoot}"
+}
+
+Show-Output "PowerShell profile configuration is ready."
